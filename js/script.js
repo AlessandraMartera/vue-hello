dea13d4d66d5b6375ea87888cef456ed0892c162
alessandra martera <ale.martera@gmail.com>
2023-05-18 14:07:33 +0200

    print a text on browser

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+const { createApp } = Vue;
+
+
+createApp({
+    data() {
+      return {
+        msg: 'Questo è il mio messaggio generato con il data',
+      }
+    }
+  }).mount('#container');
+
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
